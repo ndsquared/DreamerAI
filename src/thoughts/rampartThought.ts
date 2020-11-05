08b46eee80244589cac7946e0d7129c53ad4ed3d
Daniel Longeuay <dannylongeuay@gmail.com>
2020-11-05 06:06:33 +0000

    more ramparts

--- src/thoughts/rampartThought.ts
+++ src/thoughts/rampartThought.ts
@@ -8,11 +8,28 @@ export class RampartThought extends BuildThought {
 
   public ponder(): void {
     const spawn = this.idea.spawn;
+    // Protect spawn
     this.idea.addBuild([spawn.pos], STRUCTURE_RAMPART, 3);
+
+    // Protect controller
     const controller = this.idea.spawn.room.controller;
     if (controller) {
       const controllerNeighbors = controller.pos.availableNeighbors(true);
       this.idea.addBuild(controllerNeighbors, STRUCTURE_RAMPART, 4);
     }
+
+    // Protect towers
+    const towers = spawn.room.find(FIND_STRUCTURES, {
+      filter: s => {
+        if (s.structureType === STRUCTURE_TOWER) {
+          return true;
+        }
+        return false;
+      }
+    });
+    const towerPos = _.map(towers, t => t.pos);
+    this.idea.addBuild(towerPos, STRUCTURE_RAMPART, 5);
+
+    // Protect base
   }
 }
