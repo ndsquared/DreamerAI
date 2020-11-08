01f555fa5bfd2f2499c1049f48975e819db070ca
Daniel Longeuay <dannylongeuay@gmail.com>
2020-11-08 07:48:41 +0000

    harvesters should just harvest

--- src/thoughts/harvestThought.ts
+++ src/thoughts/harvestThought.ts
@@ -37,16 +37,9 @@ export class HarvestThought extends FigmentThought {
     const containers = this.source.pos.findInRange(FIND_STRUCTURES, 1, {
       filter: s => s.structureType === STRUCTURE_CONTAINER
     });
-    let constructionSites: ConstructionSite[] | null = null;
 
     let shouldDropHarvest = false;
-    let shouldRepairContainer = false;
-    let shouldBuildContainer = false;
     if (containers.length > 0) {
-      const container = containers[0];
-      if (container.hits < container.hitsMax) {
-        shouldRepairContainer = true;
-      }
       if (containers[0].pos.isEqualTo(figment.pos)) {
         shouldDropHarvest = true;
       } else {
@@ -59,23 +52,7 @@ export class HarvestThought extends FigmentThought {
         }
       }
     } else {
-      constructionSites = this.source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1, {
-        filter: s => s.structureType === STRUCTURE_CONTAINER
-      });
-      if (this.name === FigmentThoughtName.REMOTE_HARVEST) {
-        if (constructionSites.length) {
-          shouldBuildContainer = true;
-        } else {
-          const buildPositions = this.source.pos.availableNeighbors(true);
-          if (buildPositions.length) {
-            const buildPos = buildPositions[0];
-            const result = this.source.room.createConstructionSite(buildPos, STRUCTURE_CONTAINER);
-            if (result !== OK) {
-              console.log(`${figment.name} unable to create container at ${buildPos.toString()}`);
-            }
-          }
-        }
-      } else if (this.name === FigmentThoughtName.HARVEST) {
+      if (this.name === FigmentThoughtName.HARVEST) {
         shouldDropHarvest = this.idea.getFigmentCount(FigmentThoughtName.TRANSFER) > 0;
       }
     }
@@ -88,23 +65,13 @@ export class HarvestThought extends FigmentThought {
     if (links.length) {
       useLink = true;
     }
-    if (shouldDropHarvest && !useLink && !shouldRepairContainer && !shouldBuildContainer) {
+    if (shouldDropHarvest && !useLink) {
       targetOptions = {
         ignoreFigmentCapacity: true
       };
     }
     if (figment.store.getUsedCapacity() === 0) {
       figment.addNeuron(NeuronType.HARVEST, this.source.id, this.source.pos, targetOptions);
-    } else if (shouldRepairContainer) {
-      const container = containers[0];
-      figment.addNeuron(NeuronType.REPAIR, container.id, container.pos);
-    } else if (shouldBuildContainer) {
-      if (constructionSites && constructionSites.length) {
-        const constructionSite = constructionSites[0];
-        figment.addNeuron(NeuronType.BUILD, constructionSite.id, constructionSite.pos);
-      } else {
-        console.log(`unable to find constuction site near ${this.sourcePos.toString()}`);
-      }
     } else {
       const target = figment.getNextTransferTargetNeighborhood({ useLink, originRoom: this.idea.spawn.room });
       if (target) {
